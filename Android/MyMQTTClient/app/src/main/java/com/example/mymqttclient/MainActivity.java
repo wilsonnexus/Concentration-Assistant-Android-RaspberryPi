e3a33ee76fcadd7f1123de0d14ea7dbcda0dcbce
wilsonnexus <96637419+wilsonnexus@users.noreply.github.com>
2022-11-10 06:54:23 -0500

    Added Android MQTT Code

---
+++ Android/MyMQTTClient/app/src/main/java/com/example/mymqttclient/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.mymqttclient;
+
+//import android.support.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+//import com.github.mikephil.charting.charts.LineChart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mymqttclient.helper.MqttHelper;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+//import helpers.ChartHelper;
+//import helpers.MqttHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    MqttHelper mqttHelper;
+    //ChartHelper mChart;
+    //LineChart chart;
+
+    TextView dataReceived;
+    private Button btnPublish;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dataReceived = (TextView) findViewById(R.id.dataReceived);
+        //chart = (LineChart) findViewById(R.id.chart);
+        //mChart = new ChartHelper(chart);
+
+        startMqtt();
+        /*btnPublish = (Button) findViewById(R.id.btnPublish);
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    //Log.d(TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    //Log.d(TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+        btnPublish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String topic = "foo/bar";
+                String payload = "the payload";
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
+    }
+
+
+    private void startMqtt(){
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                Log.w("Debug","Connected");
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+                dataReceived.setText(mqttMessage.toString());
+                //mChart.addEntry(Float.valueOf(mqttMessage.toString()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+}
