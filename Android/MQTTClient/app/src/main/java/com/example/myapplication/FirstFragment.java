7a626b3e597011aaf8abc6192f9412d576e3c64f
wilsonnexus <96637419+wilsonnexus@users.noreply.github.com>
2023-05-18 10:30:00 -0400

    Overdrive and Pomodoro Update

--- Android/MQTTClient/app/src/main/java/com/example/myapplication/FirstFragment.java
+++ Android/MQTTClient/app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -1,16 +1,7 @@
 package com.example.myapplication;
 
-import android.app.Activity;
-import android.app.ActivityOptions;
-import android.app.admin.DevicePolicyManager;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,50 +12,27 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.myapplication.databinding.FragmentFirstBinding;
 
-import android.os.Parcel;
 import android.widget.Button;
-import android.widget.Toast;
 
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-
-
-
-public class FirstFragment extends Fragment{
+public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
-    // Allowlist two apps.
-    private static final String KIOSK_PACKAGE = "com.example.myapplication";
-    //private static final String PLAYER_PACKAGE = "com.example.player";
-    private static final String[] APP_PACKAGES = {KIOSK_PACKAGE};
-    // ...
-    static final int RESULT_ENABLE = 1;
-    DevicePolicyManager devicePolicyManager;
-    ComponentName componentName;
-    Button btn_unblock;
-    Button btn_block;
-    Context context;
-
+    private Button pomodoroButton;
+    private Button overdriveButton;
+    //private Button buttonFirst;
+    //private Button buttonGoBack;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-
-
-
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        //buttonFirst = binding.buttonFirst;
 
-
-
+        //buttonFirst.setVisibility(View.GONE); // hide the button initially
+        //buttonFirst = (Button) findViewById(R.id.button_first);
         return binding.getRoot();
-
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
@@ -75,149 +43,52 @@ public class FirstFragment extends Fragment{
             }
         });
 
-
-
-
-       /*binding.buttonUnblock.setOnClickListener(new View.OnClickListener() {
-        //btn_unblock.setOnClickListener(new View.OnClickListener() {
+        // This button enables you to go back to MainActivity
+        binding.goBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                hhh.disableBackButton();
-
-
-            }
-        });*/
-
-
-        /*binding.buttonUnblock.setOnClickListener(new View.OnClickListener() {
-            //btn_unblock.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {*/
-                //dpm.setLockTaskPackages(adminName, );
-                /*boolean active=devicePolicyManager.isAdminActive(componentName);
-                if(active){
-                    devicePolicyManager.removeActiveAdmin(componentName);
-                    btn_unblock.setText("Enable");
-                    btn_block.setVisibility(View.GONE);
-                }else{
-                    Intent intent=new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,componentName);
-                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"You Should Enable the app!");
-
-                    //   startActivityForResult(intent,RESULT_ENABLE);
-                    Log.i("Helllooooo", "Not working");
-                    activityResultLaunch.launch(intent);
-
-                }*/
-            /*}
-        });*/
-
-
-
-
-        /*binding.buttonBlock.setOnClickListener(new View.OnClickListener() {
-            //btn_block.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //devicePolicyManager.lockNow();
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    // Set an option to turn on lock task mode when starting the activity.*/
-                    /*ActivityOptions options = ActivityOptions.makeBasic();
-                    options.setLockTaskEnabled(true);
-
-                    // Start our kiosk app's main activity with our lock task mode option.
-                    PackageManager packageManager = context.getPackageManager();
-                    Intent launchIntent = packageManager.getLaunchIntentForPackage(KIOSK_PACKAGE);
-                    if (launchIntent != null) {
-                        context.startActivity(launchIntent, options.toBundle());
-                    }
-                }
-                else {
-                        // First, confirm that this package is allowlisted to run in lock task mode.
-                        if (devicePolicyManager.isLockTaskPermitted(context.getPackageName())) {
-                            getActivity().startLockTask();
-                        } else {
-                            // Because the package isn't allowlisted, calling startLockTask() here
-                            // would put the activity into screen pinning mode.
-                        }
+                if (getActivity() instanceof MainActivity) {
+                    ((MainActivity) getActivity()).showMainContent();
                 }
             }
-        });*/
-
-/*        binding.buttonBlock.setOnClickListener(new View.OnClickListener() {
-            //btn_block.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                devicePolicyManager.lockNow();
-*/
-                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    // Set an option to turn on lock task mode when starting the activity.
-                    ActivityOptions options = ActivityOptions.makeBasic();
-                    options.setLockTaskEnabled(true);
-
-                    // Start our kiosk app's main activity with our lock task mode option.
-                    PackageManager packageManager = context.getPackageManager();
-                    Intent launchIntent = packageManager.getLaunchIntentForPackage(KIOSK_PACKAGE);
-                    if (launchIntent != null) {
-                        context.startActivity(launchIntent, options.toBundle());
-                    }
-                }
-                else {
-                        // First, confirm that this package is allowlisted to run in lock task mode.
-                        if (devicePolicyManager.isLockTaskPermitted(context.getPackageName())) {
-                            getActivity().startLockTask();
-                        } else {
-                            // Because the package isn't allowlisted, calling startLockTask() here
-                            // would put the activity into screen pinning mode.
-                        }
-                }*/
-/*            }
         });
 
-*/
+        // This button enables you to set the timer to the Pomodoro Technique Mode
+        pomodoroButton = binding.pomodoroButton;
+        pomodoroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getActivity() instanceof MainActivity) {
+                    ((MainActivity) getActivity()).startPomodoroTimer();
+                }
+            }
+        });
+
+        // This button enables you to set the timer to the Overdrive Mode
+        overdriveButton = binding.overdriveButton;
+        overdriveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getActivity() instanceof MainActivity) {
+                    ((MainActivity) getActivity()).startOverdriveTimer();
+                }
+            }
+        });
 
     }
 
-
-
-
-    ActivityResultLauncher<Intent> activityResultLaunch = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        // ToDo : Do your stuff...
-                        btn_unblock.setText("Dissable");
-                        btn_block.setVisibility(View.VISIBLE);
-                        Toast.makeText(context, "Dissabled", Toast.LENGTH_SHORT).show();
-                    } else {
-                        // ToDo : Do your stuff...
-                        Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-
-                    }
-                }
-            });
-
-    /*@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode){
-            case RESULT_ENABLE:
-                if(resultCode== Activity.RESULT_OK ){
-                    btn_unblock.setText("Dissable");
-                    btn_block.setVisibility(View.VISIBLE);
-                    Toast.makeText(context, "Dissabled", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-                }
-                return;
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getActivity() != null) {
+            MainActivity activity = (MainActivity) getActivity();
+            // Show the button only when the title of the action bar is "Settings"
 
         }
-        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /*public Button getButtonFirst() {
+        return buttonFirst;
     }*/
 
     @Override
@@ -225,7 +96,4 @@ public class FirstFragment extends Fragment{
         super.onDestroyView();
         binding = null;
     }
-
-
-
 }
--- Android/MQTTClient/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/MQTTClient/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -31,10 +33,12 @@ import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 * Author: Wilson Neira
-* Desciprion: This app is used along a Raspberry Pi vibration motor
+* Description: This app is used along a Raspberry Pi vibration motor
 * and EEG to alert a user when they have stopped concentrating on a task.
 * */
 
@@ -45,13 +49,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private DevicePolicyManager devicePolicyManager;
     private ActivityManager activityManager;
     private ComponentName componentName;
-
+    //public static boolean settingsClicked = false; // Declare boolean variable here
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     MqttHelper mqttHelper;
 
     TextView dataReceived;
     private Button btnPublish;
+    private boolean overdrive = true;
 
 
     // Allowlist one app
@@ -71,28 +76,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        View firstfragmentContent = findViewById(R.id.fragment_first);
+        firstfragmentContent.setVisibility(View.GONE);
+
         //setContentView(R.layout.activity_main);
         devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
         activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         componentName = new ComponentName(this, Controller.class);
 
-
-
-        //lock = (Button) findViewById(R.id.lock);
-        //enable = (Button) findViewById(R.id.enable);
         disable = (Button) findViewById(R.id.disable);
         remainingTimeTextView = findViewById(R.id.textView);
         durationSpinner = findViewById(R.id.durationSpinner);
-        //Log.d("TAGGER", R.array.durations_array);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.durations_array, android.R.layout.simple_spinner_item);
 
+        // Replace the ArrayAdapter initialization code with the new code
+        String[] durations = getResources().getStringArray(R.array.durations_array);
+        List<String> durationsList = new ArrayList<>();
+        for (String duration : durations) {
+            int durationValue = Integer.parseInt(duration);
+            if (durationValue == 1) {
+                durationsList.add(duration + " minute");
+            }
+            else {
+                durationsList.add(duration + " minutes");
+            }
+        }
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.custom_spinner_item, durationsList);
+
+        adapter.setDropDownViewResource(R.layout.custom_spinner_item);
         durationSpinner.setAdapter(adapter);
 
-        //lock.setOnClickListener(this);
-        //enable.setOnClickListener(this);
+
         disable.setOnClickListener(this);
 
         dataReceived = (TextView) findViewById(R.id.textview_first);
@@ -105,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onBackPressed() {
         if (backButtonEnabled) {
+
             super.onBackPressed();
         }
     }
@@ -128,91 +145,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        /*if (view == lock) {
-            boolean active = devicePolicyManager.isAdminActive(componentName);
-            if(true) {
-                //devicePolicyManager.lockNow();
-                //disableBackButton();
-                startLockTask();
-                mIsLocked = true;
-                // Disable the "screen pinned mode" message
-                //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                 //   devicePolicyManager.setLockTaskFeatures(componentName, DevicePolicyManager.LOCK_TASK_FEATURE_HOME);
-                //}
-            } else {
-                Toast.makeText(this, "You need to enable the Admin Device Features", Toast.LENGTH_SHORT).show();
-            }
-        } else if (view == enable) {
-            stopLockTask();
-            mIsLocked = false;
+     if (view == disable) {
+         if (!overdrive) {
+             pomodoroTechnique();
+             //overdriveImplementation();
+         }
+         else {
+             overdriveImplementation();
+         }
 
-
-        }*/ if (view == disable) {
-                if (!mIsLocked) {
-                    // First click
-                    int delayDuration = Integer.parseInt(durationSpinner.getSelectedItem().toString()) * 60000;
-
-                    // Update a TextView to show the time remaining
-                    // (optional)
-                    // Execute first action when timer is done
-                    disable.setText("Cancel");
-                    boolean active = devicePolicyManager.isAdminActive(componentName);
-                    if (true) {
-                        //devicePolicyManager.lockNow();
-                        //disableBackButton();
-                        startLockTask();
-                        mIsLocked = true;
-
-                    } //else {
-                    //Toast.makeText(this, "You need to enable the Admin Device Features", Toast.LENGTH_SHORT).show();
-                    //}
-
-                    // Create a new timer for 5 seconds (adjust as desired)
-                    timer = new CountDownTimer(delayDuration, 1000) {
-                        public void onTick(long millisUntilFinished) {
-                            // Update a TextView to show the time remaining
-                            // (optional)
-                            long remainingTime = millisUntilFinished / 1000;
-                            remainingTimeTextView.setText("Time Left: " + remainingTime);
-                        }
-
-                        public void onFinish() {
-                            // Execute second action when timer is done
-                            disable.setText("Disable");
-                            stopLockTask();
-                            mIsLocked = false;
-                            remainingTimeTextView.setText("");
-                        }
-                    };
-                    timer.start(); // Start the new timer
-                } else {
-                    // Second click
-                    disable.setText("Disable");
-                    timer.cancel();
-                    stopLockTask();
-                    mIsLocked = false;
-                    remainingTimeTextView.setText("0");
-                    // Turn vibration motor on
-                    mqttHelper.publishToTopic("sensor/temp", "1");
-                    /*try {
-                        // Wait for 5 seconds
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }*/
-                    // Turn vibration motor off
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            mqttHelper.publishToTopic("sensor/temp", "0");
-                        }
-                    }, 5000);
-                    // mqttHelper.publishToTopic("sensor/temp", "0");
-                }
-               /* }
-            };
-            timer.start(); // Start the initial */
         }
 
     }
@@ -242,10 +183,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            // Find the main activity content and hide it
+            View mainActivityContent = findViewById(R.id.main_activity_content);
+            mainActivityContent.setVisibility(View.GONE);
+
+            // Navigate to the FirstFragment (SettingsFragment) when the settings button is clicked
+            Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.FirstFragment);
+            //settingsClicked = true; // set settingsClicked to true
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void openSettings(View view) {
+
+        // Find the main activity content and hide it
+        View mainActivityContent = findViewById(R.id.main_activity_content);
+        mainActivityContent.setVisibility(View.GONE);
+
+        // Navigate to the FirstFragment (SettingsFragment) when the settings button is clicked
+        Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.FirstFragment);
+        toggleNavHostFragment(true);
+    }
+
+
+
+   public void toggleNavHostFragment(boolean show) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment navHostFragment = fragmentManager.findFragmentById(R.id.nav_host_fragment);
+
+        if (show) {
+            fragmentManager.beginTransaction()
+                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                    .show(navHostFragment)
+                    .commit();
+        } else {
+            fragmentManager.beginTransaction()
+                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                    .hide(navHostFragment)
+                    .commit();
+        }
+    }
+
+    public void showMainContent() {
+        // Find the main activity content and show it
+        View mainActivityContent = findViewById(R.id.main_activity_content);
+        mainActivityContent.setVisibility(View.VISIBLE);
+
+        // Hide the NavHostFragment
+        toggleNavHostFragment(false);
     }
 
     @Override
@@ -256,8 +245,157 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    // Start MQTT Client Code
+    // Modes
+    public void startPomodoroTimer() {
+        // Implement the Pomodoro timer logic
+        // For example, you can use the existing timer logic with some modifications
+        overdrive = false;
+    }
 
+    public void startOverdriveTimer() {
+        // Implement the Overdrive timer logic
+        // For example, you can use the existing timer logic without breaks
+        overdrive = true;
+
+    }
+
+    public void overdriveImplementation() {
+        if (!mIsLocked) {
+            // First click
+            int delayDuration = Integer.parseInt(durationSpinner.getSelectedItem().toString().replaceAll("[^\\d.]", "")) * 60000;
+
+            // Update a TextView to show the time remaining
+            // (optional)
+            // Execute first action when timer is done
+            disable.setText("Cancel");
+            boolean active = devicePolicyManager.isAdminActive(componentName);
+            if (true) {
+                //devicePolicyManager.lockNow();
+                //disableBackButton();
+                startLockTask();
+                mIsLocked = true;
+
+            }
+
+            // Create a new timer for 5 seconds (adjust as desired)
+            timer = new CountDownTimer(delayDuration, 1000) {
+                public void onTick(long millisUntilFinished) {
+                    // Update a TextView to show the time remaining
+                    // (optional)
+                    long remainingTime = millisUntilFinished / 1000;
+                    remainingTimeTextView.setText("Time Left: " + remainingTime);
+                }
+
+                public void onFinish() {
+                    // Execute second action when timer is done
+                    disable.setText("Disable");
+                    stopLockTask();
+                    mIsLocked = false;
+                    remainingTimeTextView.setText("");
+                }
+            };
+            timer.start(); // Start the new timer
+        } else {
+
+
+            // Second click
+            disable.setText("Disable");
+            timer.cancel();
+            stopLockTask();
+            mIsLocked = false;
+            remainingTimeTextView.setText("0");
+            // Turn vibration motor on
+            mqttHelper.publishToTopic("sensor/temp", "1");
+
+            // Turn vibration motor off
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mqttHelper.publishToTopic("sensor/temp", "0");
+                }
+            }, 5000);
+            // mqttHelper.publishToTopic("sensor/temp", "0");
+        }
+    }
+
+
+    public void pomodoroTechnique() {
+        if (!mIsLocked) {
+            // First click
+            int workDuration = Integer.parseInt(durationSpinner.getSelectedItem().toString().replaceAll("[^\\d.]", "")) * 60000;
+            int breakDuration = 5000; // Set your desired break duration in milliseconds
+
+            // Lock the app (blocking)
+            lockApp();
+
+            // Create a new timer for the work period
+            timer = new CountDownTimer(workDuration, 1000) {
+                public void onTick(long millisUntilFinished) {
+                    long remainingTime = millisUntilFinished / 1000;
+                    remainingTimeTextView.setText("Work Time Left: " + remainingTime);
+                }
+
+                public void onFinish() {
+                    // Unlock the app (unblocking)
+                    unlockApp();
+
+                    // Create a new timer for the break period
+                    timer = new CountDownTimer(breakDuration, 1000) {
+                        public void onTick(long millisUntilFinished) {
+                            long remainingTime = millisUntilFinished / 1000;
+                            remainingTimeTextView.setText("Break Time Left: " + remainingTime);
+                        }
+
+                        public void onFinish() {
+                            // Re-lock the app (blocking) and restart the work timer
+                            lockApp();
+                            timer.start();
+                        }
+                    };
+                    timer.start(); // Start the break timer
+                }
+            };
+            timer.start(); // Start the work timer
+        } else {
+            // Second click
+            unlockApp();
+            timer.cancel();
+            remainingTimeTextView.setText("0");
+        }
+    }
+
+    private void lockApp() {
+        disable.setText("Cancel");
+        boolean active = devicePolicyManager.isAdminActive(componentName);
+        if (true) {
+            startLockTask();
+            mIsLocked = true;
+        }
+    }
+
+    private void unlockApp() {
+        disable.setText("Disable");
+        stopLockTask();
+        mIsLocked = false;
+        remainingTimeTextView.setText("");
+        // Turn vibration motor on
+        mqttHelper.publishToTopic("sensor/temp", "1");
+
+        // Turn vibration motor off
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mqttHelper.publishToTopic("sensor/temp", "0");
+            }
+        }, 5000);
+    }
+
+
+
+
+    // Start MQTT Client Code
     private void startMqtt(){
         mqttHelper = new MqttHelper(getApplicationContext());
         mqttHelper.mqttAndroidClient.setCallback(new MqttCallbackExtended() {
@@ -283,6 +421,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
-
     }
 }
